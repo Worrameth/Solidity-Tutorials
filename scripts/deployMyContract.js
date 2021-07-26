021095d1f6cc9431a6201f0715cd0235269e88eb
Worrameth <Worrameth666@gmail.com>
2021-07-27 00:44:46 +0700

    My First Solidity Project

---
+++ scripts/deployMyContract.js
@@ -0,0 +1,13 @@
+async function main(){
+  const MyContract = await ethers.getContractFactory("MyContract");
+  const myContract = await MyContract.deploy("My Contract");
+
+  console.log("My Contract Deployed to:",myContract.address);
+}
+
+main()
+  .then(() => {process.exit(0)})
+  .catch(err => {
+    console.error(error);
+    process.exit(1);
+  });
